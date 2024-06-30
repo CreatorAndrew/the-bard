ca631ae740b04670639ebc9833b7d5822755b577
Andrew Mitchell <andrew.noah.mitchell@gmail.com>
2024-06-30 14:00:10 -0500

    update requirements for PyPy

---
+++ FixPyPy.py
@@ -0,0 +1,13 @@
+import platform
+import requests
+
+if platform.python_implementation() == "PyPy":
+    from os.path import abspath, dirname
+
+    module_path = f"{dirname(abspath(requests.__file__))}/../pomice"
+    files = ["pool", "applemusic/client", "spotify/client"]
+    for file in files:
+        to_read = open(f"{module_path}/{file}.py", "r")
+        contents = to_read.read().replace("orjson", "json")
+        to_write = open(f"{module_path}/{file}.py", "w")
+        to_write.write(contents)
