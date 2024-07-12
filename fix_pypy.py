84e314a4ebdb49ccdd297eaa918623f85db9a279
Andrew Mitchell <andrew.noah.mitchell@gmail.com>
2024-07-12 13:12:57 -0500

    improve fix_pypy.py

--- fix_pypy.py
+++ fix_pypy.py
@@ -1,13 +1,12 @@
 import platform
 
 if platform.python_implementation() == "PyPy":
-    import requests
-    from os.path import abspath, dirname
+    from importlib.util import find_spec
+    from os.path import dirname
 
-    module_path = f"{dirname(abspath(requests.__file__))}/../pomice"
-    files = ["pool", "applemusic/client", "spotify/client"]
-    for file in files:
-        to_read = open(f"{module_path}/{file}.py", "r")
-        contents = to_read.read().replace("orjson", "json")
-        to_write = open(f"{module_path}/{file}.py", "w")
-        to_write.write(contents)
+    module_path = dirname(find_spec("pomice").origin)
+    for file in ["pool", "applemusic/client", "spotify/client"]:
+        contents = (
+            open(f"{module_path}/{file}.py", "r").read().replace("orjson", "json")
+        )
+        open(f"{module_path}/{file}.py", "w").write(contents)
